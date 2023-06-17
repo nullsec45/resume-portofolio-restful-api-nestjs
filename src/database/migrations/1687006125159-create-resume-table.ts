c631dfe74fff21e4c56588157c0ba6e742b9d99d
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-06-17 21:21:04 +0700

    feat(migrations): initial commit for resume table migration

---
+++ src/database/migrations/1687006125159-create-resume-table.ts
@@ -0,0 +1,78 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateResumeTable1687006125159 implements MigrationInterface {
+  name = 'CreateResumeTable1687006125159';
+
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'resume',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'userId',
+            type: 'int',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'age',
+            type: 'int',
+          },
+          {
+            name: 'profilePicture',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'datetime',
+            precision: 6,
+            default: 'CURRENT_TIMESTAMP(6)',
+          },
+          {
+            name: 'updatedAt',
+            type: 'datetime',
+            precision: 6,
+            default: 'CURRENT_TIMESTAMP(6)',
+            onUpdate: 'CURRENT_TIMESTAMP(6)',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'resume',
+      new TableForeignKey({
+        columnNames: ['userId'],
+        referencedTableName: 'user',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    const table = (await queryRunner.getTable('resume')) as Table;
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('userId') !== -1,
+    ) as TableForeignKey;
+
+    await queryRunner.dropForeignKey('resume', foreignKey);
+    await queryRunner.dropTable('resume');
+  }
+}
