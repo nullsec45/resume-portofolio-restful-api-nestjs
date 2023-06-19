8c78f2c4bcf5fac2039c0c1f681fff5c92e43f7e
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-06-19 22:53:40 +0700

    docs(auth): add missing extra models

--- src/auth/docs/api-auth.doc.ts
+++ src/auth/docs/api-auth.doc.ts
@@ -2,9 +2,18 @@ import { applyDecorators } from '@nestjs/common';
 import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { SuccessResponseDto } from '../../common/response/dto/success-response.dto';
 import { FailedResponseDto } from '../../common/response/dto/failed-response.dto';
+import { JwtUserPayloadDto } from '../dto/jwt-user-payload.dto';
+import { SuccessfulSignInDto } from '../dto/successful-sign-in.dto';
+import { User } from '../../users/entities/users.entity';
 
 export const ApiAuth = () =>
   applyDecorators(
     ApiTags('auth'),
-    ApiExtraModels(SuccessResponseDto, FailedResponseDto),
+    ApiExtraModels(
+      SuccessResponseDto,
+      FailedResponseDto,
+      JwtUserPayloadDto,
+      SuccessfulSignInDto,
+      User,
+    ),
   );
