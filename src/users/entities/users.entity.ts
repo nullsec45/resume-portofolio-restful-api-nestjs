fc75bf849f58f906046d5a31a3dc2686b60cbe2d
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-06-19 15:37:00 +0700

    docs(users): add user entity code docs with example

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -9,26 +9,57 @@ import {
 } from 'typeorm';
 import { Resume } from '../../resumes/entities/resumes.entity';
 import { Exclude } from 'class-transformer';
+import { ApiHideProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
+  /**
+   * User ID.
+   *
+   * @example 1
+   */
   @PrimaryGeneratedColumn()
   id: number;
 
+  /**
+   * Unique username for the user.
+   *
+   * @example "johndoe123"
+   */
   @Index({ unique: true })
   @Column()
   username: string;
 
+  /**
+   * User's password (encrypted or hashed).
+   *
+   * @example "12345678"
+   */
   @Column()
   @Exclude()
   password: string;
 
+  /**
+   * Date and time when the user was created.
+   *
+   * @example "2023-06-19T12:30:00Z"
+   */
   @CreateDateColumn()
   createdAt: Date;
 
+  /**
+   * Date and time when the user was last updated.
+   *
+   * @example "2023-06-19T15:45:00Z"
+   */
   @UpdateDateColumn()
   updatedAt: Date;
 
+  /**
+   * List of resumes associated with the user.
+   */
   @OneToMany(() => Resume, (resume) => resume.user)
+  @Exclude()
+  @ApiHideProperty()
   resumes: Resume[];
 }
