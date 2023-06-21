ca531716dc9fb188f8d3514c2a6f5999ce860994
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-06-21 18:07:50 +0700

    fix(multer): remove acl

--- src/common/multer/multer.service.ts
+++ src/common/multer/multer.service.ts
@@ -25,7 +25,6 @@ export class MulterConfigService implements MulterOptionsFactory {
         storage: multerS3({
           s3,
           bucket,
-          acl: 'public-read',
           contentType: multerS3.AUTO_CONTENT_TYPE,
           metadata: (req, file, cb) => {
             cb(null, { fieldName: file.fieldname });
